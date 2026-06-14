49f4f2040b1055bd2a62593d92400a7fefb59ba8
Abhinav Pandey <abhinavpandey56393@gmail.com>
2026-06-15 00:44:01 +0530

    update

--- Java/LinkedList/Main.java
+++ Java/LinkedList/Main.java
@@ -5,8 +5,12 @@ public class Main {
         LinkedList ll = new LinkedList();
         ll.insertAtFirst(5);
         ll.insertAtFirst(6);
-        ll.insertAtFirst(7);
-        ll.insertAtFirst(8);
+        // ll.insertAtFirst(7);
+        // ll.insertAtFirst(8);
+        ll.insertAtLast(8);
+        ll.insertAtLast(9);
+        ll.insertAtLast(10);
+        ll.insertAtIndex(2, 12);
         ll.display();
 
     }
@@ -24,9 +28,12 @@ class Node {
 
 class LinkedList {
     Node head;
+    Node tail;
+    int size = 0;
 
     public LinkedList() {
         head = null;
+        tail = null;
     }
 
     void insertAtFirst(int value) {
@@ -35,13 +42,41 @@ class LinkedList {
             head = temp;
             head.data = value;
             head.next = null;
+            tail = head;
         } else {
             temp.next = head;
             head = temp;
         }
-
+        size++;
     }
-
+    void insertAtLast(int value){
+        Node temp = new Node(value);
+        if(head == null){
+            head = temp;
+            head.data = value;
+            head.next = null;
+            tail = head;
+        }
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+    void insertAtIndex(int index,int value){
+        Node temp = new Node(value);
+        if(index > size - 1){
+            System.err.println("Index Out of Bound");
+            return;
+        }
+        Node newTemp = head;
+        for(int i = 0;i < index - 1;i++){
+            newTemp = newTemp.next;
+        }
+        temp.next = newTemp.next;
+        newTemp.next = temp;
+        size++;
+    }
     void display() {
         Node temp = head;
         while (temp != null) {
